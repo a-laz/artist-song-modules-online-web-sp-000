6c96a93d9efa3119f82d4dbb42d623dea6223496
Alex Lazarev <alex.m.lazarev@gmail.com>
2020-04-02 04:34:03 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,7 +1,5 @@
 module Memorable
-
   module ClassMethods
-
     def reset_all
       self.all.clear
     end
@@ -9,15 +7,11 @@ module Memorable
     def count
       self.all.count
     end
-
   end
 
   module InstanceMethods
-
     def initialize
       self.class.all << self
     end
-
   end
-
 end
